b22972c31d0df4a52044f376d021b695787134f1
xrysav25 <xrysav25@vutbr.cz>
2021-10-20 05:50:20 +0200

    ADD code changes in main

--- Labs/04-interrupts/interrupts/interrupts/main.c
+++ Labs/04-interrupts/interrupts/interrupts/main.c
@@ -15,6 +15,7 @@
 #define LED_D2  PB4
 #define LED_D3  PB3
 #define LED_D4  PB2
+#define BUTTON  PD0
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
@@ -34,11 +35,13 @@ int main(void)
     // Configuration of LED(s) at port B
     GPIO_config_output(&DDRB, LED_D1);
     GPIO_write_low(&PORTB, LED_D1);
-
+	GPIO_config_input_pullup(&DDRD, BUTTON);
     // Configuration of 16-bit Timer/Counter1 for LED blinking
     // Set the overflow prescaler to 262 ms and enable interrupt
     TIM1_overflow_262ms();
     TIM1_overflow_interrupt_enable();
+	
+	
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -48,6 +51,7 @@ int main(void)
     {
         /* Empty loop. All subsequent operations are performed exclusively 
          * inside interrupt service routines ISRs */
+		if (GPIO_read(&PIND,button)) {TIM1_overflow_33ms();}  else{ TIM1_overflow_262ms();}
     }
 
     // Will never reach this
