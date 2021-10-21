ef65202d44b5741164b99c801dcd7163032dd90e
student <student@PC-636.urel.feec.vutbr.cz>
2021-10-21 10:50:59 +0200

    ADD lab 5 code

---
+++ Labs/05-segments/05-segment/05-segment/segment.c
@@ -0,0 +1,109 @@
+/***********************************************************************
+ * 
+ * Seven-segment display library for AVR-GCC.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2019-Present Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/* Includes ----------------------------------------------------------*/
+#ifndef F_CPU
+# define F_CPU 16000000
+#endif
+#include <util/delay.h>
+#include "gpio.h"
+#include "segment.h"
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: SEG_init()
+ * Purpose:  Configure SSD signals LATCH, CLK, and DATA as output.
+ * Returns:  none
+ **********************************************************************/
+void SEG_init(void)
+{
+    /* Configuration of SSD signals */
+    GPIO_config_output(&DDRD, SEG_LATCH);
+    GPIO_config_output(&DDRD, SEG_CLK);
+    GPIO_config_output(&DDRB, SEG_DATA);
+}
+
+/**********************************************************************
+ * Function: SEG_update_shift_regs()
+ * Purpose:  Display segments at one position of the SSD.
+ * Input:    segments - Segments to be displayed (abcdefgDP, active low) - 8bit digit
+ *           position - Position of the display where the segments are to - 1000 -> p3 aktivní
+ *                      be displayed (p3 p2 p1 p0 xxxx, active high)
+ * Returns:  none
+ **********************************************************************/
+void SEG_update_shift_regs(uint8_t segments, uint8_t position)
+{
+    uint8_t bit_number;
+
+    // Pull LATCH, CLK, and DATA low
+    GPIO_write_low(&PORTD, SEG_LATCH);
+    GPIO_write_low(&PORTD, SEG_CLK);
+    GPIO_write_low(&PORTB, SEG_DATA);
+    // Wait 1 us
+    _delay_us(1);
+    // Loop through the 1st byte (segments)
+    // a b c d e f g DP (active low values)
+    for (bit_number = 0; bit_number < 8; bit_number++)
+    {
+        // Test LSB of "segments" by & (faster) or % (slower) and... 
+        // ...output DATA value
+       
+        if ((segments & 1) == 1)
+        {GPIO_write_high(&PORTB, SEG_DATA);}
+        else
+        {GPIO_write_low(&PORTB, SEG_DATA);}
+        // Wait 1 us
+        _delay_us(1);
+        // Pull CLK high
+        GPIO_write_high(&PORTD, SEG_CLK);
+        // Wait 1 us
+        _delay_us(1);
+        // Pull CLK low
+        GPIO_write_low(&PORTD, SEG_CLK);
+        // Shift "segments"
+        segments = segments >> 1; //shifting register
+    }
+
+    // Loop through the 2nd byte (position)
+    // p3 p2 p1 p0 . . . . (active high values)
+    for (bit_number = 0; bit_number < 8; bit_number++)
+    {
+        // Test LSB of "position" by & (faster) or % (slower) and... 
+        // ...output DATA value
+        if ((position & 1) == 1)
+        {GPIO_write_high(&PORTB, SEG_DATA);}
+        else
+        {GPIO_write_low(&PORTB, SEG_DATA);}
+        // Wait 1 us
+        _delay_us(1);
+        // Pull CLK high
+        GPIO_write_high(&PORTD, SEG_CLK);
+        // Wait 1 us
+        _delay_us(1);
+        // Pull CLK low
+        GPIO_write_low(&PORTD, SEG_CLK);
+        // Shift "position"
+        position = position >> 1;       
+    }
+
+    // Pull LATCH high
+    GPIO_write_high(&PORTD, SEG_LATCH);
+    // Wait 1 us
+    _delay_us(1);
+}
+
+/**********************************************************************
+ * Function: SEG_clear()
+ **********************************************************************/
+
+/**********************************************************************
+ * Function: SEG_clk_2us()
+ **********************************************************************/
